3f209bd806fc74984cd3c5472203a54fb3ae3182
saikrishnareddykatta <45930992+saikrishnareddykatta@users.noreply.github.com>
2020-06-18 15:37:40 -0500

    commit

--- ICP 4/Source/knn.py
+++ ICP 4/Source/knn.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sklearn.svm import SVC, LinearSVC
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 
 train_df = pd.read_csv('./train_preprocessed.csv')
@@ -8,6 +9,7 @@ X_train = train_df.drop("Survived",axis=1)
 Y_train = train_df["Survived"]
 X_test = test_df.drop("PassengerId",axis=1).copy()
 print(train_df[train_df.isnull().any(axis=1)])
+
 # KNN Classifier
 knn = KNeighborsClassifier(n_neighbors = 3)
 knn.fit(X_train, Y_train)
@@ -15,12 +17,10 @@ Y_pred = knn.predict(X_test)
 acc_knn = round(knn.score(X_train, Y_train) * 100, 2)
 
 # finding the KNN Accuracy
-print("KNN accuracy is:",acc_knn)
-print(train_df[['Sex', 'Survived']]. \
-      groupby(['Sex']). \
-      mean().sort_values(by='Survived', ascending=False))
+print("KNN accuracy is:", acc_knn)
+print(train_df[['Sex', 'Survived']].groupby(['Sex']).mean().sort_values(by='Survived', ascending=False))
+# visualising the data
+g = sns.FacetGrid(train_df, col="Survived")
+g.map(plt.hist, 'Sex', bins=10)
+plt.show()
 
-# Using the Correlation method
-data = train_df[['Sex', 'Survived']]
-correlation = data.corr(method='pearson')
-print(correlation)
